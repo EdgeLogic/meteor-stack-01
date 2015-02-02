fad5c3ece05f6d1cf2e8a2290f39d534f33ee16d
bitcoins.sg <bitcoinssg@gmail.com>
2015-02-03 01:02:39 +0545

    install all basic on help dir

--- client/main-client.js
+++ client/main-client.js
@@ -1,3 +1,5 @@
+// client only
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -16,8 +18,3 @@ if (Meteor.isClient) {
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
---
+++ server/main-server.js
@@ -0,0 +1,7 @@
+// server only
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
